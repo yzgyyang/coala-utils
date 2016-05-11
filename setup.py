dd103c47931a4ee747657ee587b72fb5ec411c66
adrian.zatreanu <zgadi_2007@yahoo.com>
2016-05-11 22:17:59 +0300

    Update to version 0.1.1
    This adds __init__.py to the package.

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'coala-decorators',
   packages = ['coala-decorators'],
-  version = '0.1',
+  version = '0.1.1',
   description = 'A collection of useful decorators.',
   author = 'Adrian Zatreanu',
   author_email = 'adrian.zatreanu@eestec.ro',
   url = 'https://github.com/coala-analyzer/coala-decorators',
-  download_url = 'https://www.github.com/coala-analyzer/coala-decorators/tarball/0.1',
+  download_url = 'https://www.github.com/coala-analyzer/coala-decorators/tarball/0.1.1',
   keywords = ['coala', 'decorators', 'bears'],
   classifiers = [],
 )

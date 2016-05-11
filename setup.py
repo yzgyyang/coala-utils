c5957d4046c5d88f63d4698aae982dc311655556
adrian.zatreanu <zgadi_2007@yahoo.com>
2016-05-11 21:25:01 +0300

    setup.py: Add it

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'coala-decorators',
+  packages = ['coala-decorators'],
+  version = '0.1',
+  description = 'All the coala decorators',
+  author = 'Adrian Zatreanu',
+  author_email = 'adrian.zatreanu@eestec.ro',
+  url = 'https://github.com/coala-analyzer/coala-decorators',
+  download_url = 'https://www.github.com/coala-analyzer/coala-decorators/tarball/0.1',
+  keywords = ['coala', 'decorators', 'bears'],
+  classifiers = [],
+)

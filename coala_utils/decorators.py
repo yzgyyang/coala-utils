6d7595a9374ba9f71d7e2714bb81bb1f6eb758f8
Adrian Zatreanu <zgadi_2007@yahoo.com>
2016-07-04 14:00:34 +0300

    decorators.py: Correct docs

--- coala_utils/decorators.py
+++ coala_utils/decorators.py
@@ -108,11 +108,11 @@ def _construct_repr_string(obj, members):
 
 def get_public_members(obj):
     """
-    Retrieves a list of member-like objects (members or properties) that are
+    Retrieves a dict of member-like objects (members or properties) that are
     publically exposed.
 
     :param obj: The object to probe.
-    :return:    A list of strings.
+    :return:    A dict of strings, {member : value}.
     """
     return {attr: getattr(obj, attr) for attr in dir(obj)
             if not attr.startswith("_")

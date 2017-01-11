fdda137d21e2a74b6e29468e83f199bb9b7f92b3
vijeth-aradhya <vijthaaa@gmail.com>
2017-01-11 13:29:29 +0100

    string_processing: Add join_names
    Related to https://github.com/coala/coala/issues/3486

--- coala_utils/string_processing/__init__.py
+++ coala_utils/string_processing/__init__.py
@@ -5,5 +5,5 @@ from coala_utils.string_processing.Core import (
      search_for, unescaped_search_for, split, unescaped_split,
      search_in_between, unescaped_search_in_between, nested_search_in_between,
      escape, convert_to_raw, unescape, unescaped_rstrip, unescaped_strip,
-     position_is_escaped)
+     position_is_escaped, join_names)
 # Stop ignoring

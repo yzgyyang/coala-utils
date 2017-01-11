28fed426524fdbc5a963073eb06d298ecaa58aff
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2017-01-11 13:44:12 +0100

    param_conversion: Add test

--- coala_utils/param_conversion.py
+++ coala_utils/param_conversion.py
@@ -1,2 +1,12 @@
 def negate(a):
+    """
+    Negates the given thing!
+
+    >>> negate(True)
+    False
+    >>> negate('')
+    True
+
+    :param a: Anything that can be evaluated as a bool!
+    """
     return not a

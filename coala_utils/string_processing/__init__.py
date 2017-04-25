6c213f51819a3164f237468c4a42471823a191a1
Guilherme Leobas <guilhermeleobas@gmail.com>
2017-04-24 23:17:06 -0300

    Fix incorrect indentation level in string_processing/__init__.py

--- coala_utils/string_processing/__init__.py
+++ coala_utils/string_processing/__init__.py
@@ -2,8 +2,8 @@
 from coala_utils.string_processing.Match import Match
 from coala_utils.string_processing.InBetweenMatch import InBetweenMatch
 from coala_utils.string_processing.Core import (
-     search_for, unescaped_search_for, split, unescaped_split,
-     search_in_between, unescaped_search_in_between, nested_search_in_between,
-     escape, convert_to_raw, unescape, unescaped_rstrip, unescaped_strip,
-     position_is_escaped, join_names)
+    search_for, unescaped_search_for, split, unescaped_split,
+    search_in_between, unescaped_search_in_between, nested_search_in_between,
+    escape, convert_to_raw, unescape, unescaped_rstrip, unescaped_strip,
+    position_is_escaped, join_names)
 # Stop ignoring

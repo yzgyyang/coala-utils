f0d74e8167edef7b69c3ec6ef4a8f0b3bdb13844
Guangyuan Yang <yzgyyang@outlook.com>
2018-06-13 17:36:35 -0400

    Question.py: Add function to ask yes/no questions
    This adds a helper to specifically ask and handle yes/no questions,
    useful for situations when a user confirmation is needed.
    
    Closes https://gitlab.com/coala/coala-utils/issues/59

--- coala_utils/Question.py
+++ coala_utils/Question.py
@@ -1,4 +1,5 @@
 from pyprint.ConsolePrinter import ConsolePrinter
+from coala_utils.Constants import TRUE_STRINGS, FALSE_STRINGS
 from coala_utils.string_processing.StringConverter import StringConverter
 
 
@@ -44,3 +45,47 @@ def ask_question(question,
                 color="blue")
         else:
             return answer
+
+
+def ask_yes_no(question,
+               default=None,
+               printer=ConsolePrinter()):
+    """
+    Asks the user a yes/no question until the user gives a clear answer,
+    and returns a boolean value representing the answer.
+
+    :param question:
+        String to be used as question.
+    :param default:
+        The default answer to be returned if the user gives a void answer
+        to the question. It must be one of ('yes', 'no', None).
+    :param printer:
+        The printer object used for console interactions. If this is not
+        given, it defaults to a ``ConsolePrinter``.
+    :return:
+        True if the user input is one of coala-utils.Constants.TRUE_STRING,
+        False if the user input is one of coala-utils.Constant.FALSE_STRING.
+        The user input is case insensitive.
+    """
+    if default is None:
+        prompt = ' [y/n] '
+    elif default == 'yes':
+        prompt = ' [Y/n] '
+    elif default == 'no':
+        prompt = ' [y/N] '
+    else:
+        raise ValueError('Invalid default answer: %s' % default)
+
+    while True:
+        printer.print(question + prompt, color='yellow', end=' ')
+        answer = input().lower().strip()
+
+        if default and len(answer) == 0:
+            answer = default
+
+        if answer in TRUE_STRINGS:
+            return True
+        elif answer in FALSE_STRINGS:
+            return False
+        else:
+            printer.print('Invalid answer, please try again.', color='red')
--- tests/QuestionTest.py
+++ tests/QuestionTest.py
@@ -3,7 +3,7 @@ import unittest
 from pyprint.ConsolePrinter import ConsolePrinter
 from coala_utils.ContextManagers import (
     simulate_console_inputs, suppress_stdout, retrieve_stdout)
-from coala_utils.Question import ask_question
+from coala_utils.Question import ask_question, ask_yes_no
 
 
 class TestQuestion(unittest.TestCase):
@@ -52,3 +52,77 @@ class TestQuestion(unittest.TestCase):
 
             self.assertIn("Please enter a", custom_stdout.getvalue())
             self.assertEqual(response, 42)
+
+
+class TestYesNo(unittest.TestCase):
+
+    def setUp(self):
+        self.printer = ConsolePrinter()
+        self.question_text = 'Are you sure?'
+
+    def test_ask_yes_no(self):
+        with simulate_console_inputs('yes'), retrieve_stdout() as custom_stdout:
+            response = ask_yes_no(
+                self.question_text,
+                printer=self.printer)
+            self.assertIn(
+                self.question_text,
+                custom_stdout.getvalue())
+            self.assertIn(
+                ' [y/n] ',
+                custom_stdout.getvalue())
+            self.assertTrue(response)
+
+    def test_yes_no_default(self):
+        with simulate_console_inputs(''), retrieve_stdout() as custom_stdout:
+            response = ask_yes_no(
+                self.question_text,
+                default='no',
+                printer=self.printer)
+            self.assertIn(
+                self.question_text,
+                custom_stdout.getvalue())
+            self.assertIn(
+                ' [y/N] ',
+                custom_stdout.getvalue())
+            self.assertFalse(response)
+
+    def test_yes_no_change_default(self):
+        with simulate_console_inputs('No'), retrieve_stdout() as custom_stdout:
+            response = ask_yes_no(
+                self.question_text,
+                default='yes',
+                printer=self.printer)
+            self.assertIn(
+                self.question_text,
+                custom_stdout.getvalue())
+            self.assertIn(
+                ' [Y/n] ',
+                custom_stdout.getvalue())
+            self.assertFalse(response)
+
+    def test_yes_no_loop(self):
+        with simulate_console_inputs('', 'Noo', 'q', 'y'), \
+                retrieve_stdout() as custom_stdout:
+            response = ask_yes_no(
+                self.question_text,
+                default=None,
+                printer=self.printer)
+            self.assertEqual(
+                3,
+                custom_stdout.getvalue().count('Invalid'))
+            self.assertEqual(
+                4,
+                custom_stdout.getvalue().count(self.question_text))
+            self.assertEqual(
+                4,
+                custom_stdout.getvalue().count(' [y/n] '))
+            self.assertTrue(response)
+
+    def test_yes_no_invalid_default(self):
+        with simulate_console_inputs('y'):
+            with self.assertRaises(ValueError):
+                ask_yes_no(
+                    self.question_text,
+                    default='nahh',
+                    printer=self.printer)

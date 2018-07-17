ac382613bb2f62eaac8b9e68b35421ae43dd93eb
Saksham Bansal <sakshambansal1999@yahoo.com>
2018-07-17 16:48:12 +0700

    __init__.py: Use __all__ for imports
    Closes https://gitlab.com/coala/coala-utils/issues/33

--- coala_utils/string_processing/__init__.py
+++ coala_utils/string_processing/__init__.py
@@ -1,4 +1,11 @@
 # Start ignoring PyImportSortBear because of dependency chains!
+
+__all__ = ('Match', 'InBetweenMatch', 'search_for', 'unescaped_search_for',
+           'split', 'unescaped_split', 'search_in_between',
+           'unescaped_search_in_between', 'nested_search_in_between', 'escape',
+           'convert_to_raw', 'unescape', 'unescaped_rstrip', 'unescaped_strip',
+           'position_is_escaped', 'join_names')
+
 from coala_utils.string_processing.Match import Match
 from coala_utils.string_processing.InBetweenMatch import InBetweenMatch
 from coala_utils.string_processing.Core import (
